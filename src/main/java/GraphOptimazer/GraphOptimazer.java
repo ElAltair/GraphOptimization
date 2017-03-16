7eecdcfad5debf3dc287e2b943ac6b3f980fefdc
Vladislav <vkhakin@gmail.com>
2017-03-17 01:14:32 +0300

    changed printMatrix functions, added formatter and line and row counter

--- src/main/java/GraphOptimazer/GraphOptimazer.java
+++ src/main/java/GraphOptimazer/GraphOptimazer.java
@@ -1,19 +1,13 @@
 package GraphOptimazer;
 import cern.colt.function.DoubleDoubleFunction;
-import cern.colt.function.DoubleFunction;
 import cern.colt.matrix.DoubleFactory2D;
 import cern.colt.matrix.DoubleMatrix1D;
 import cern.colt.matrix.DoubleMatrix2D;
 import cern.colt.matrix.linalg.EigenvalueDecomposition;
-import com.sun.tools.javac.util.Pair;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
-import java.util.stream.Stream;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class GraphOptimazer {
@@ -36,12 +30,16 @@ public class GraphOptimazer {
         firstGraph = new double[graphPoints][graphPoints];
         secondGraph = new double[graphPoints][graphPoints];
         fillGraph();
-        printMatrix(graph);
+        printMatrix(graph, true,true);
     }
 
     public void fillGraph() {
+
+        /*
        new ArrayList<Position>() {
             {
+
+                // normal graph
                 add(new Position(1, 7));
                 add(new Position(2, 3));
                 add(new Position(2, 8));
@@ -78,93 +76,231 @@ public class GraphOptimazer {
                 add(new Position(14, 15));
                 add(new Position(15, 8));
                 add(new Position(15, 14));
+                }
+            /*
+           // without 4 point
+           add(new Position(1, 6));
+           add(new Position(2, 3));
+           add(new Position(2, 7));
+           add(new Position(3, 2));
+           add(new Position(3, 8));
+           add(new Position(4, 10));
+           add(new Position(5, 6));
+           add(new Position(5, 12));
+           add(new Position(6, 5));
+           add(new Position(6, 1));
+           add(new Position(6, 13));
+           add(new Position(7, 2));
+           add(new Position(7, 8));
+           add(new Position(7, 14));
+           add(new Position(8, 7));
+           add(new Position(8, 9));
+           add(new Position(8, 3));
+           add(new Position(9, 8));
+           add(new Position(9, 10));
+           add(new Position(10, 4));
+           add(new Position(10, 9));
+           add(new Position(11, 12));
+           add(new Position(12, 11));
+           add(new Position(12, 13));
+           add(new Position(12, 5));
+           add(new Position(13, 6));
+           add(new Position(13, 12));
+           add(new Position(13, 14));
+           add(new Position(14, 7));
+           add(new Position(14, 13));
+       }
+        }.forEach((elem)->{
+            graph[elem.getRowIndex() - 1][elem.getColumnIndex() - 1] = 1.0;
+        });
+
+
+        */
+
+
+        new ArrayList<Position>() {
+            {
+                add(new Position(1, 2));
+                add(new Position(1, 3));
+                add(new Position(2, 1));
+                add(new Position(2, 4));
+                add(new Position(3, 4));
+                add(new Position(3, 1));
+                add(new Position(3, 6));
+                add(new Position(4, 2));
+                add(new Position(4, 3));
+                add(new Position(4, 7));
+                add(new Position(5, 6));
+                add(new Position(6, 3));
+                add(new Position(6, 5));
+                add(new Position(6, 7));
+                add(new Position(7, 6));
+                add(new Position(7, 4));
+                add(new Position(7, 16));
+                add(new Position(8, 12));
+                add(new Position(8, 9));
+                add(new Position(9, 8));
+                add(new Position(9, 10));
+                add(new Position(9, 13));
+                add(new Position(10, 9));
+                add(new Position(10, 11));
+                add(new Position(10, 14));
+                add(new Position(11, 10));
+                add(new Position(11, 15));
+                add(new Position(12, 8));
+                add(new Position(12, 16));
+                add(new Position(12, 13));
+                add(new Position(13, 12));
+                add(new Position(13, 14));
+                add(new Position(13, 9));
+                add(new Position(14, 13));
+                add(new Position(14, 15));
+                add(new Position(14, 10));
+                add(new Position(15, 14));
+                add(new Position(15, 11));
+                add(new Position(16, 7));
+                add(new Position(16, 12));
             }
         }.forEach((elem)->{
             graph[elem.getRowIndex() - 1][elem.getColumnIndex() - 1] = 1.0;
         });
 
-       /*
-       new ArrayList<Position>() {
-            {
-                add(new Position(1, 3));
-                add(new Position(1, 5));
-                add(new Position(2, 4));
-                add(new Position(2, 6));
-                add(new Position(3, 1));
-                add(new Position(3, 4));
-                add(new Position(3, 5));
-                add(new Position(4, 2));
-                add(new Position(4, 3));
-                add(new Position(4, 6));
-                add(new Position(5, 1));
-                add(new Position(5, 3));
-                add(new Position(6, 2));
-                add(new Position(6, 4));
-            }
-        }.forEach((elem)->{
-            graph[elem.getRowIndex() - 1][elem.getColumnIndex() - 1] = 1.0;
-        });
-        */
     }
 
     public void createGraphs(ArrayList<Integer> firstGraphPoints, ArrayList<Integer> secondGraphPoints){
 
     }
 
-    public void printMatrix(double[][] matrix) {
+    public void printMatrix(double[][] matrix, boolean useColorizedOutput, boolean useFormatter) {
+        final AtomicInteger idx = new AtomicInteger();
+        System.out.format("%5s", "");
+        for(int i = 0; i < matrix.length; ++i)
+            System.out.format("%5s", i + 1);
+        System.out.println();
         Arrays.stream(matrix).forEach((row) -> {
+            System.out.format("%5s", idx.incrementAndGet() + " |");
             Arrays.stream(row).forEach(elem -> {
-                if (elem == 1.0)
-                    System.out.print("\u001B[31m " + elem + " \u001B[0m");
-                else
-                    System.out.print(elem + " ");
+                if (useFormatter) {
+                    if (elem == 1.0) {
+                        if (useColorizedOutput) {
+                            System.out.print("\u001B[31m");
+                            System.out.format("%5.1f", elem);
+                            System.out.print("\u001B[0m");
+                        } else
+                            System.out.format("%5.1f", elem);
+                    } else
+                        System.out.format("%5.1f", elem);
+                }
+                else{
+                    if (elem == 1.0) {
+                        if (useColorizedOutput) {
+                            System.out.print("\u001B[31m");
+                            System.out.print(elem);
+                            System.out.print("\u001B[0m");
+                        } else
+                            System.out.print(elem);
+                    } else
+                        System.out.print(elem);
+                }
             });
             System.out.println();
         });
     }
 
-    public void printMatrix(double[] matrix, boolean useColorizedOutput){
+    public void printMatrix(double[] matrix, boolean useColorizedOutput, boolean useFormatter){
+        final AtomicInteger idx = new AtomicInteger();
         Arrays.stream(matrix).forEach((elem) -> {
-                if (elem == 1.0)
-                    if(useColorizedOutput)
-                        System.out.print("\u001B[31m " + elem + " \u001B[0m\n");
-                    else
-                        System.out.print(elem + "\n");
-
-                else
-                    System.out.print(elem + "\n");
-            });
-            System.out.println();
+            System.out.format("%5s" , + idx.incrementAndGet() +  " |");
+            if (useFormatter) {
+                if (elem == 1.0) {
+                    if (useColorizedOutput) {
+                        System.out.print("\u001B[31m");
+                        System.out.format("%5.1f%n", elem);
+                        System.out.print("\u001B[0m");
+                    } else
+                        System.out.format("%5.1f%n", elem);
+                } else
+                    System.out.format("%5.1f%n", elem);
+            }
+            else{
+                if (elem == 1.0) {
+                    if (useColorizedOutput) {
+                        System.out.print("\u001B[31m");
+                        System.out.println(elem);
+                        System.out.print("\u001B[0m");
+                    } else
+                        System.out.println(elem);
+                } else
+                    System.out.println(elem);
+            }
+        });
+        System.out.println();
     }
 
-    public void printMatrix(DoubleMatrix2D matrix, boolean useColorizedOutput){
-        for(int i = 0; i < matrix.rows(); ++i) {
-            for(int j = 0; j < matrix.columns(); ++j) {
-                if(matrix.get(i,j) != 0.0)
-                    if(useColorizedOutput)
-                        System.out.print("\u001B[31m " + matrix.get(i,j) + " \u001B[0m");
-                    else
-                        System.out.print(matrix.get(i,j) + " ");
-
-                else
-                    System.out.print(matrix.get(i,j) + " ");
+    public void printMatrix(DoubleMatrix2D matrix, boolean useColorizedOutput, boolean useFormatter) {
+        System.out.format("%5s", "");
+        for(int i =0; i < matrix.columns(); ++i)
+            System.out.format("%5s", i + 1);
+        System.out.println();
+        for (int i = 0; i < matrix.rows(); ++i) {
+            System.out.format("%5s" ,i + 1 + " |");
+            for (int j = 0; j < matrix.columns(); ++j) {
+                if (useFormatter) {
+                    if (matrix.get(i,j) != 0.0) {
+                        if (useColorizedOutput) {
+                            System.out.print("\u001B[31m");
+                            System.out.format("%5.1f", matrix.get(i,j));
+                            System.out.print("\u001B[0m");
+                        } else
+                            System.out.format("%5.1f", matrix.get(i,j));
+                    } else
+                        System.out.format("%5.1f", matrix.get(i,j));
+                }
+                else{
+                    if (matrix.get(i,j) == 1.0) {
+                        if (useColorizedOutput) {
+                            System.out.print("\u001B[31m");
+                            System.out.print(matrix.get(i,j));
+                            System.out.print("\u001B[0m");
+                        } else
+                            System.out.print(matrix.get(i,j));
+                    } else
+                        System.out.print(matrix.get(i,j));
+                }
             }
             System.out.println();
         }
     }
 
-    public void printMatrix(DoubleMatrix1D matrix, boolean useColorizedOutPut){
+    public void printMatrix(DoubleMatrix1D matrix, boolean useColorizedOutput, boolean useFormatter){
         for(int i = 0; i < matrix.size(); ++i) {
-                if(matrix.get(i) != 0.0)
-                    if(useColorizedOutPut)
-                        System.out.print("\u001B[31m " + matrix.get(i) + " \u001B[0m\n");
-                    else
-                        System.out.print(matrix.get(i) + "\n");
-                else
-                    System.out.print(matrix.get(i) + "\n");
-            }
-            System.out.println();
+            if(matrix.get(i) != 0.0)
+                System.out.format("%5s" ,i + 1 + " | ");
+                if (useFormatter) {
+                    if (matrix.get(i) == 1.0) {
+                        if (useColorizedOutput) {
+                            System.out.print("\u001B[31m");
+                            System.out.format("%5.1f%n", matrix.get(i));
+                            System.out.print("\u001B[0m");
+                        } else
+                            System.out.format("%5.1f", matrix.get(i));
+                    } else
+                        System.out.format("%5.1f%n", matrix.get(i));
+                }
+                else{
+                    if (matrix.get(i) == 1.0) {
+                        if (useColorizedOutput) {
+                            System.out.print("\u001B[31m");
+                            System.out.println(matrix.get(i));
+                            System.out.print("\u001B[0m");
+                        } else
+                            System.out.println(matrix.get(i));
+                    } else
+                        System.out.println(matrix.get(i));
+                }
         }
+        System.out.println();
+    }
 
     public void optimize(){
         DoubleFactory2D matrixFactory = DoubleFactory2D.dense;
@@ -189,7 +325,7 @@ public class GraphOptimazer {
             }
         };
         DoubleMatrix2D LMatrix = BMatrix.assign(AMatrix, substract);
-        printMatrix(LMatrix, true);
+        printMatrix(LMatrix, true, true);
 
         EigenvalueDecomposition eigenvalueDecomposition = new EigenvalueDecomposition(LMatrix);
         DoubleMatrix1D UVector = eigenvalueDecomposition.getV().viewColumn(1);
@@ -199,6 +335,9 @@ public class GraphOptimazer {
         ArrayList<Integer> firstGraphPoints = new ArrayList<>();
         ArrayList<Integer> secondGraphPoints = new ArrayList<>();
         double[] uVector = UVector.toArray();
+        System.out.println("UAvg = " + UAvg);
+        System.out.println("UVector = ");
+        printMatrix(UVector,false,false);
         boolean nextFirstGraph = true;
         for(int i = 0 ; i < uVector.length; ++i){
             nextFirstGraph = firstGraphPoints.size() <= secondGraphPoints.size();
@@ -206,6 +345,7 @@ public class GraphOptimazer {
                 firstGraphPoints.add(i + 1);
             else if(uVector[i] > UAvg)
                 secondGraphPoints.add(i + 1);
+
             else {
                 if(nextFirstGraph)
                     firstGraphPoints.add(i + 1);
@@ -213,6 +353,7 @@ public class GraphOptimazer {
                     secondGraphPoints.add(i + 1);
 
             }
+
         }
 
         //printMatrix(uVector,false);
--- src/main/java/GraphOptimazer/MainClass.java
+++ src/main/java/GraphOptimazer/MainClass.java
@@ -1,10 +1,8 @@
 package GraphOptimazer;
 
-import cern.colt.matrix.DoubleFactory2D;
-
 public class MainClass {
     public static void main(String[] args){
-        GraphOptimazer graphOptimazer = new GraphOptimazer(15);
+        GraphOptimazer graphOptimazer = new GraphOptimazer(16);
         graphOptimazer.optimize();
     }
 }
